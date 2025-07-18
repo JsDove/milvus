1cd3d25f93823945649dca7334a8bc60455bc634
Xianhui.Lin <xianhui.lin@zilliz.com>
2025-07-18 16:42:45 +0800

    enhance: add growingBf
    Signed-off-by: Xianhui.Lin <xianhui.lin@zilliz.com>

--- internal/flushcommon/pipeline/flow_graph_dd_node.go
+++ internal/flushcommon/pipeline/flow_graph_dd_node.go
@@ -202,7 +202,7 @@ func (ddn *ddNode) Operate(in []Msg) []Msg {
 				WithLabelValues(fmt.Sprint(paramtable.GetNodeID()), metrics.InsertLabel).
 				Add(float64(imsg.GetNumRows()))
 
-			log.Debug("DDNode receive insert messages",
+			log.Info("DDNode receive insert messages",
 				zap.Int64("segmentID", imsg.GetSegmentID()),
 				zap.String("channel", ddn.vChannelName),
 				zap.Int("numRows", len(imsg.GetRowIDs())),
@@ -221,7 +221,7 @@ func (ddn *ddNode) Operate(in []Msg) []Msg {
 				continue
 			}
 
-			log.Debug("DDNode receive delete messages",
+			log.Info("DDNode receive delete messages",
 				zap.String("channel", ddn.vChannelName),
 				zap.Int64("numRows", dmsg.NumRows),
 				zap.Uint64("startPosTs", msMsg.StartPositions()[0].GetTimestamp()),
