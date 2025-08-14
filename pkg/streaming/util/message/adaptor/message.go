d47ac39be4e5e02fad36def8641952230f886b86
Xianhui.Lin <xianhui.lin@zilliz.com>
2025-08-14 14:51:05 +0800

    test
    Signed-off-by: Xianhui.Lin <xianhui.lin@zilliz.com>

--- pkg/streaming/util/message/adaptor/message.go
+++ pkg/streaming/util/message/adaptor/message.go
@@ -264,15 +264,11 @@ func recoverDeleteMsgFromHeader(deleteMsg *msgstream.DeleteMsg, header *message.
 	if deleteMsg.GetCollectionID() != header.GetCollectionId() {
 		panic("unreachable code, collection id is not equal")
 	}
-	timestamps := make([]uint64, len(header.GetSegmentIds()))
+	timestamps := make([]uint64, len(deleteMsg.Timestamps))
 	for i := 0; i < len(timestamps); i++ {
 		timestamps[i] = timetick
 	}
-	deleteMsg.SegmentIds = header.GetSegmentIds()
-	deleteMsg.PrimaryKeys = header.GetDeletePrimaryKeys()
 	deleteMsg.Timestamps = timestamps
-	deleteMsg.NumRows = int64(len(header.GetSegmentIds()))
-	log.Info("recoverDeleteMsgFromHeader segmentIds", zap.Int("segmentIds", len(deleteMsg.SegmentIds)), zap.Int("timestamps", len(deleteMsg.Timestamps)))
 	return deleteMsg, nil
 }
 
